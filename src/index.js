7334e11d17c2116ed96d1b62ef01439a5925d25b
AxxpN <pedrohfs575@gmail.com>
2021-09-03 23:07:11 -0300

    calculadora

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Primeiro from './componentes/primeiro'
+import Bomdia from './componentes/bomdia'
+import { BoaTarde, BoaNoite } from './componentes/multiplos'
+import Saudacao from './componentes/saudacao'
+import Botoes from './componentes/botoes'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div>
+         <Botoes/> 
+    </div>
+    , document.getElementById('root'))
